fbd0c814d69c8319f65f2858f8b0ce597024cefb
Maxim Yudayev <maxim.yudayev@gmail.com>
2025-10-22 04:42:13 +0200

    feat: installable package

---
+++ examples/dots.bat
@@ -0,0 +1,1 @@
+call hermes-cli -o .\ --config_file dots.yml --experiment project=Test type=DOTs trial=0
---
+++ examples/dots.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+hermes-cli -o ./ --config_file dots.yml --experiment project=Test type=DOTs trial=0
